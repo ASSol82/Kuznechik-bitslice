cd68efcc29a5207a6c0b9dc19529b1d4a20ee73f
sol <soloviov-anatoly@mail.ru>
2025-06-01 12:16:25 +0300

    CTCrypt 2025 20250601_1

--- Convert_bitslice.h
+++ Convert_bitslice.h
@@ -1,18 +1,1 @@
-#ifndef __CONVERT_BITSLICE__
-#define __CONVERT_BITSLICE__
-
-
-#if MaxCountMessage==256
-
-
-#include <stdint.h>
-#include <immintrin.h>
-
-
-void ConvertBitslice_32x32(const __m256i w256[4], uint32_t* p32, const uint32_t offset);
-
-
-#endif //MaxCountMessage==256
-
-
-#endif //__CONVERT_BITSLICE__
+/media/320Gb/Projects/Kuznechik/src/src_Kuznechik_bislice/Convert_bitslice.h
